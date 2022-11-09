44602fe29dba036c9fb14b1e320530382b6e0e0b
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-11-09 20:43:34 +0600

    review based on email filtered

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -18,8 +18,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('photographyService').collection('services')
+        const reviewCollection = client.db('photographyService').collection('reviews')
 
-
+        // show all services in service page 
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
@@ -27,11 +28,73 @@ async function run() {
             res.send(services)
         })
 
+        // show details services in details page 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        // show 3 services in home page 
         app.get('/homeservice', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
             const homeservice = await cursor.limit(3).toArray()
             res.send(homeservice)
+        })
+
+
+        // app.get('/reviews', async (req, res) => {
+        //     const query = {}
+        //     const cursor = reviewCollection.find(query)
+        //     const reviews = await cursor.toArray()
+        //     console.log(reviews)
+        //     res.send(reviews)
+        // })
+
+        // get all default review 
+        // app.get('/allreviews', async (req, res) => {
+        //     const query = {}
+        //     const cursor = reviewCollection.find(query)
+        //     const reviews = await cursor.toArray()
+        //     res.send(reviews)
+        // })
+
+
+        // get all review 
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            const cursor = reviewCollection.find(query)
+            const reviews = await cursor.toArray()
+            console.log(reviews);
+            res.send(reviews)
+        })
+
+
+        // get all my review 
+        app.get('/addreview', async (req, res) => {
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const cursor = reviewCollection.find(query)
+            const addreview = await cursor.toArray()
+            res.send(addreview)
+        })
+
+        // post review
+        app.post('/addreview', async (req, res) => {
+            const reviews = req.body
+            console.log(reviews);
+            const result = await reviewCollection.insertOne(reviews)
+
+            res.send(result)
         })
 
 
@@ -43,6 +106,15 @@ async function run() {
 
             res.send(result)
         })
+
+        // to create review 
+        // app.post('/reviews', async (req, res) => {
+        //     const reviews = req.body
+        //     console.log(reviews);
+        //     const result = await reviewCollection.insertOne(reviews)
+
+        //     res.send(result)
+        // })
 
 
     }
@@ -62,5 +134,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Genius car server running on ${port}`);
+    console.log(`PETER McKinnon photography  server running on ${port}`);
 })
